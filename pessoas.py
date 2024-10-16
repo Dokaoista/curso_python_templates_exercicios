b05214a2291313457e7ec6f74941ccd7e4982775
Doka <83892184+Dokaoista@users.noreply.github.com>
2024-10-16 11:02:40 -0300

    Update pessoas.py

--- pessoas.py
+++ pessoas.py
@@ -1,10 +1,1 @@
-class Pessoas:
-    def __init__(self) -> None:
-        return "Iniciando..."
-    
-    def calculadora(numeros, numeros2):
-        numeros_total = numeros+numeros2
-        return numeros_total
-    
-    def capturar_pokemon(player, pokemon):
-        return f"{player} capturou {pokemon}!"
+
